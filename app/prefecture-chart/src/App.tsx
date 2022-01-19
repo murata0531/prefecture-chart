b3f83c291ca3614585ed9bd5d79caca3fb552a24
murata0531 <coless115@gmail.com>
2022-01-19 13:40:48 +0900

    apiにアクセスするためのパラメータの型を定義

--- app/prefecture-chart/src/App.tsx
+++ app/prefecture-chart/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import Prefectures from './Prefectures'
+import axios from 'axios'
 
+// 都道府県のコードと名前を定義
+interface prefectures {
+	prefCode: number,
+	prefName: string,
+	isSelected: boolean
+}
+
+// 都道府県ごとの人口を定義
+interface populations {
+	prefCode: number,
+	data: Array<data>,
+}
+
+// 年度ごとの値を定義
+interface data {
+	year: number,
+	value: number
+}
+
+// APIにアクセスするURL
+interface compositionUrl {
+	prefCode: number,
+	url: string
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
