b4ddb9231f2bd462d6cfcb2b1cc7dbd3ba1b86e6
murata0531 <coless115@gmail.com>
2022-01-19 13:20:26 +0900

    都道府県のチェックボックスをリストで表示する部品

---
+++ app/prefecture-chart/src/Prefectures.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Prefectures.scss'
+import PrefecturesItem from './PrefecturesItem'
+interface prefectures {
+	prefCode: number,
+	prefName: string,
+	isSelected: boolean
+}
+const Prefectures = (
+	{
+		prefectures,
+		setPrefectures,
+	}: {
+		prefectures: Array<prefectures>,
+		setPrefectures: any,
+	}
+) => {
+	const togglePref = (event: any) => {
+		const target = event.target
+		const clickedTarget = prefectures.find((prefecture: prefectures) => prefecture.prefName === target.id)
+		if (clickedTarget) {
+			clickedTarget.isSelected = !clickedTarget.isSelected
+		}
+		const toggledPrefectures = prefectures.map(prefecture => prefecture)
+		setPrefectures(toggledPrefectures)
+	}
+
+	return (
+		<div className="prefecture">
+			<h2 className="prefecture-title">都道府県</h2>
+			<ul className="prefecture-list">
+				{prefectures.map((prefecture: prefectures) => {
+					return (
+						<li
+							className="prefecture-list-item"
+							key={prefecture.prefCode}
+						>
+							<PrefecturesItem
+								prefecture={prefecture}
+								togglePref={togglePref}
+							/>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
+
+export default Prefectures
